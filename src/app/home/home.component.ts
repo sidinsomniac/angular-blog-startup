cef0428a3db88537abcf412129ed8b97e5af4856
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-03 13:05:06 +0530

    Create separate home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  @Input('header') heading:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
