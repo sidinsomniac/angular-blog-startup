501cc8e047a5d2331172f5e4084d86f7cf4403de
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-03 13:25:16 +0530

    Establish routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  heading = 'Angular Blog App';
-}
+export class AppComponent {}
