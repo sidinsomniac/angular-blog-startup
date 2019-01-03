8b3be29e7103b3e057fe880a70bfb9fc6c7d059d
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-03 13:56:35 +0530

    Add about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent }
 ]
 
@@ -15,3 +18,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule {}
+export const allComponents = [HomeComponent, AboutComponent, PageNotFoundComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, allComponents } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PageNotFoundComponent
+    allComponents
   ],
   imports: [
     BrowserModule,
