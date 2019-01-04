1e6c8b34c89f7110144f63bbab05d85ecd0f27aa
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-04 20:01:46 +0530

    Add new blogpost component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BlogpostComponent } from './blogpost/blogpost.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'home/:blogpost', component: BlogpostComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent }
 ]
@@ -18,4 +20,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule {}
-export const allComponents = [HomeComponent, AboutComponent, PageNotFoundComponent];
+export const allComponents = [HomeComponent, AboutComponent, PageNotFoundComponent, BlogpostComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule, allComponents } from './app-routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
